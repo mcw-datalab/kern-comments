ad2d2ad809b40b46c7ecfb81269394914d818af4
Andrew Gleave <andrew@redrobotstudios.com>
2021-07-30 14:53:06 +0100

    Add custom version of model_to_dict

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,12 +1,36 @@
+from itertools import chain
+
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models import Model, QuerySet
-from django.forms import model_to_dict
+
+
+def model_to_dict(instance, fields=None, exclude=None):
+    """
+    Return a dict containing the data in ``instance`` suitable for passing as
+    a Form's ``initial`` keyword argument.
+
+    ``fields`` is an optional list of field names. If provided, return only the
+    named.
+
+    ``exclude`` is an optional list of field names. If provided, exclude the
+    named from the returned dict, even if they are listed in the ``fields``
+    argument.
+    """
+    opts = instance._meta
+    data = {}
+    for f in chain(opts.concrete_fields, opts.private_fields, opts.many_to_many):
+        if fields is not None and f.name not in fields:
+            continue
+        if exclude and f.name in exclude:
+            continue
+        data[f.name] = f.value_from_object(instance)
+    return data
 
 
 class ExtendedEncoder(DjangoJSONEncoder):
     def default(self, o):
         if isinstance(o, Model):
-            return model_to_dict(o)
+            return model_to_dict(o, exclude=["content_object"])
         if isinstance(o, QuerySet):
-            return [model_to_dict(x) for x in o]
+            return [model_to_dict(x, exclude=["content_object"]) for x in o]
         return super().default(o)
